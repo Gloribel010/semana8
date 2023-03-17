9feebba14084910f6ef67a3760457773996a86c4
Gloribel010 <125325837+Gloribel010@users.noreply.github.com>
2023-03-16 20:51:19 -0600

    Add files via upload
    ActSemana8

---
+++ Almacen/Almacen/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using Almacen.Models;
+
+using (AlmacenContext db = new AlmacenContext())
+{
+    Producto producto = new Producto();
+
+    Console.WriteLine("Ingresar nombre del producto");
+    producto.Nombre = Console.ReadLine();
+
+    Console.WriteLine("Ingresar la descripcion");
+    producto.Descripcion = Console.ReadLine();
+
+    Console.WriteLine("Ingresar el precio");
+    producto.Precio = Convert.ToDecimal(Console.ReadLine());
+
+    Console.WriteLine("Ingresar la cantidad de producto");
+    producto.Stock = Convert.ToInt32(Console.ReadLine());
+
+    db.Productos.Add(producto);
+    db.SaveChanges();
+
+
+}
